4b9b42468a5f93768d2e2f00de873b9a12e2654b
Crowdbotics <team@crowdbotics.com>
2020-07-14 20:51:53 +0000

    Update models

--- backend/allison_sprock_fine_18900/wsgi.py
+++ backend/allison_sprock_fine_18900/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'allison_sprock_fine_18900.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allison_sprock_fine_18900.settings")
 
 application = get_wsgi_application()
